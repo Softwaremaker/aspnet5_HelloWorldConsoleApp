4a8f292d00e7517fda3f95a2d5f00083ade56203
Softwaremaker <itnews@softwaremaker.net>
2015-09-10 01:32:27 +0000

    Change console output line

--- program.cs
+++ program.cs
@@ -4,6 +4,6 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World from ASP.NET Docker Container on Ubuntu/Azure");
     }
 }
